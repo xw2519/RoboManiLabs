77a7f9a113230c30dead7a988507bb914427cb70
Xin Wang <xw2519@ic.ac.uk>
2022-01-16 16:10:16 +0000

    synced file names

---
+++ Lab 1/matlab scripts/Lab1_1_Task3_Data_Logging_with_Step_Demand_v1.m
@@ -0,0 +1,201 @@
+% Read the position of the dynamixel horn with the torque off
+% The code executes for a given amount of time then terminates
+
+
+clc;
+clear all;
+
+lib_name = '';
+
+if strcmp(computer, 'PCWIN')
+  lib_name = 'dxl_x86_c';
+elseif strcmp(computer, 'PCWIN64')
+  lib_name = 'dxl_x64_c';
+elseif strcmp(computer, 'GLNX86')
+  lib_name = 'libdxl_x86_c';
+elseif strcmp(computer, 'GLNXA64')
+  lib_name = 'libdxl_x64_c';
+elseif strcmp(computer, 'MACI64')
+  lib_name = 'libdxl_mac_c';
+end
+
+% Load Libraries
+if ~libisloaded(lib_name)
+    [notfound, warnings] = loadlibrary(lib_name, 'dynamixel_sdk.h', 'addheader', 'port_handler.h', 'addheader', 'packet_handler.h');
+end
+
+%% ---- Control Table Addresses ---- %%
+% Control table address is different in every Dynamixel model
+
+ADDR_PRO_TORQUE_ENABLE       = 64; % Torque Enable(64) determines Torque ON/OFF. Writing ‘1’ to Toque Enable’s address will turn on the Torque           
+ADDR_PRO_GOAL_POSITION       = 116; % Sets desired position
+ADDR_PRO_PRESENT_POSITION    = 132; % Present position of the servo
+ADDR_PRO_OPERATING_MODE      = 11; % Sets between Current, Velocity, Position Control Mode 
+
+%% ---- Other Settings ---- %%
+
+% Protocol version
+PROTOCOL_VERSION            = 2.0;          % See which protocol version is used in the Dynamixel
+
+% Default setting
+DXL_ID                      = 13;            % Dynamixel ID: 1
+BAUDRATE                    = 1000000;
+DEVICENAME                  = 'COM12';       % Check which port is being used on your controller
+                                            % ex) Windows: 'COM1'   Linux: '/dev/ttyUSB0' Mac: '/dev/tty.usbserial-*'
+                                            
+TORQUE_ENABLE               = 1;            % Value for enabling the torque
+TORQUE_DISABLE              = 0;            % Value for disabling the torque
+DXL_MINIMUM_POSITION_VALUE  = -150000;      % Dynamixel will rotate between this value
+DXL_MAXIMUM_POSITION_VALUE  = 150000;       % and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+DXL_MOVING_STATUS_THRESHOLD = 20;           % Dynamixel moving status threshold
+
+ESC_CHARACTER               = 'e';          % Key for escaping loop
+
+COMM_SUCCESS                = 0;            % Communication Success result value
+COMM_TX_FAIL                = -1001;        % Communication Tx Failed
+
+% Plotting variables
+CURRENT_PLOT                = [];
+X_AXIS_COUNTER              = 0;
+
+
+%% ------------------ %%
+
+% Initialize PortHandler Structs
+% Set the port path
+% Get methods and members of PortHandlerLinux or PortHandlerWindows
+port_num = portHandler(DEVICENAME);
+
+% Initialize PacketHandler Structs
+packetHandler();
+
+index = 1;
+dxl_comm_result = COMM_TX_FAIL;           % Communication result
+dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE DXL_MAXIMUM_POSITION_VALUE];         % Goal position
+
+dxl_error = 0;                              % Dynamixel error
+dxl_present_position = 0;                   % Present position
+
+
+% Open port
+if (openPort(port_num))
+    fprintf('Port Open\n');
+else
+    unloadlibrary(lib_name);
+    fprintf('Failed to open the port\n');
+    input('Press any key to terminate...\n');
+    return;
+end
+
+
+% Set port baudrate
+if (setBaudRate(port_num, BAUDRATE))
+    fprintf('Baudrate Set\n');
+else
+    unloadlibrary(lib_name);
+    fprintf('Failed to change the baudrate!\n');
+    input('Press any key to terminate...\n');
+    return;
+end
+
+% Put actuator into Position Control Mode
+write1ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_PRO_OPERATING_MODE, 3);
+
+% Enable Dynamixel Torque
+write1ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE);
+
+dxl_comm_result = getLastTxRxResult(port_num, PROTOCOL_VERSION);
+dxl_error = getLastRxPacketError(port_num, PROTOCOL_VERSION);
+
+if dxl_comm_result ~= COMM_SUCCESS
+    fprintf('%s\n', getTxRxResult(PROTOCOL_VERSION, dxl_comm_result));
+elseif dxl_error ~= 0
+    fprintf('%s\n', getRxPacketError(PROTOCOL_VERSION, dxl_error));
+else
+    fprintf('Dynamixel has been successfully connected \n');
+end
+
+% Constantly alternate between 0deg and 180deg
+while 1
+    if input('Press any key to continue! (or input e to quit!)\n', 's') == ESC_CHARACTER
+        break;
+    end
+
+    % Write goal position
+    write4ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_PRO_GOAL_POSITION, typecast(int32(dxl_goal_position(index)), 'uint32'));
+    if getLastTxRxResult(port_num, PROTOCOL_VERSION) ~= COMM_SUCCESS
+        printTxRxResult(PROTOCOL_VERSION, getLastTxRxResult(port_num, PROTOCOL_VERSION));
+    elseif getLastRxPacketError(port_num, PROTOCOL_VERSION) ~= 0
+        printRxPacketError(PROTOCOL_VERSION, getLastRxPacketError(port_num, PROTOCOL_VERSION));
+    end
+    
+    % Read present position
+    while 1
+        dxl_present_position = read4ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_PRO_PRESENT_POSITION);
+        if getLastTxRxResult(port_num, PROTOCOL_VERSION) ~= COMM_SUCCESS
+            printTxRxResult(PROTOCOL_VERSION, getLastTxRxResult(port_num, PROTOCOL_VERSION));
+        elseif getLastRxPacketError(port_num, PROTOCOL_VERSION) ~= 0
+            printRxPacketError(PROTOCOL_VERSION, getLastRxPacketError(port_num, PROTOCOL_VERSION));
+        end
+        
+        % Plot the data on the graph
+        current_plot = [X_AXIS_COUNTER, dxl_present_position];
+        CURRENT_PLOT = cat(1, CURRENT_PLOT, current_plot); % Concatenate the array
+
+        % get min and max values of points so far
+        minVals = min(XY, [], 1);
+        maxVals = max(XY, [], 1);
+
+        % plot the data point
+        scatter(xy(1), xy(2), 'b*');
+
+        if size(XY) ~= 1
+            line([current_plot(1), CURRENT_PLOT(end-1, 1)], [current_plot(2), CURRENT_PLOT(end-1, 2)], 'Color', 'b');
+        end
+
+        hold on;
+
+        % update the axis limits dynamically
+        set(ax, {'XLim', 'YLim'}, {[minVals(1)-1 maxVals(1)+1], [minVals(2)-1 maxVals(2)+1]});
+        
+        % pause so plot can update
+        pause(0.5);
+        
+        % Print logging out
+        fprintf('[ID:%03d] GoalPos:%03d  PresPos:%03d\n', DXL_ID, dxl_goal_position(index), typecast(uint32(dxl_present_position), 'int32'));
+        
+        % Check if the current servo position meets the goal position
+        if ~(abs(dxl_goal_position(index) - typecast(uint32(dxl_present_position), 'int32')) > DXL_MOVING_STATUS_THRESHOLD)
+            break;
+        end
+        
+        X_AXIS_COUNTER = X_AXIS_COUNTER + 1;
+    end
+
+    % Change goal position
+    if index == 1
+        index = 2;
+    else
+        index = 1;
+    end
+end
+
+% Disable Dynamixel Torque
+write1ByteTxRx(port_num, PROTOCOL_VERSION, DXL_ID, ADDR_PRO_TORQUE_ENABLE, TORQUE_DISABLE);
+dxl_comm_result = getLastTxRxResult(port_num, PROTOCOL_VERSION);
+dxl_error = getLastRxPacketError(port_num, PROTOCOL_VERSION);
+if dxl_comm_result ~= COMM_SUCCESS
+    fprintf('%s\n', getTxRxResult(PROTOCOL_VERSION, dxl_comm_result));
+elseif dxl_error ~= 0
+    fprintf('%s\n', getRxPacketError(PROTOCOL_VERSION, dxl_error));
+end
+
+% Close port
+closePort(port_num);
+fprintf('Port Closed \n');
+
+% Unload Library
+unloadlibrary(lib_name);
+
+close all;
+clear all;
